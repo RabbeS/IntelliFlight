0def316d0c46d33b2874138b9273b7bc8b6a21df
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-09-09 01:54:41 +0200

    update quick-build script

--- do_flash.sh
+++ do_flash.sh
@@ -5,5 +5,5 @@ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake/arm-toolchain.cmake 
 make
 
 cd ..
-sudo bash scripts/flash_stlink.sh build/OpenFlightController_Debug.bin
+sudo bash scripts/flash_stlink.sh build/IntelliFlight_Debug.bin
 
