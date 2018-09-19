496db6329e8756fe4676dbf1abcd5feed0cf1a9a
Rabbe <Rabbe@Laptop>
2018-09-19 21:50:58 +0200

    Initialized all GPIO's of the board

--- main.cpp
+++ main.cpp
@@ -3,23 +3,60 @@
 #include <libopencm3/stm32/gpio.h>
 
 static void gpioSetup(void);
+static void gpioSet(void);      //Gpio Enum z.B SD-Card 1
+static void gpioClear(void);    //-"-
+static bool gpioGet(void);      //-"-
+
 static void timerSetup(void);
+static void timerEnable(void);
+static void timerDisable(void);
+static void setPeriod(void);
+static void setCompareVal(void);
+static void getCompareVal(void);
 
 int main(void) {
     gpioSetup();
     timerSetup();
     while (true) {
-        for (int i = 0; i < 5000000; i++) {}
+        for (int i = 0; i < 50000; i++) {}
+        gpio_toggle(GPIOC, GPIO13);
     }
 }
 
 static void gpioSetup(void){
-    
+    /** Input Setup */
+    rcc_periph_clock_enable(RCC_GPIOD);
+    gpio_mode_setup(GPIOD, GPIO_MODE_INPUT, GPIO_OTYPE_PP, GPIO0 | GPIO1 | GPIO4);
+
+    /** Output Setup */
+    rcc_periph_clock_enable(RCC_GPIOA);
+    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO2 | GPIO3 | GPIO4);
+    gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO2 | GPIO3 | GPIO4);
+
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO2);
+    gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO2);
+
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO13);
+    gpio_set_output_options(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO13);
+}
+
+static void gpioSet(void){
+
+}
+
+static void gpioClear(void){
+
+}
+
+static bool gpioGet(void){
+
 }
 
 static void timerSetup(void) {
     /** Timer 1 (Ch 1234) Setup */
-    /*Setup GPIO pins for used by the timer*/
+    /*Setup GPIO pins used by the timer*/
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO9 | GPIO11 | GPIO13 | GPIO14);
     gpio_set_af(GPIOE, GPIO_AF1, GPIO9 | GPIO11 | GPIO13 | GPIO14);
@@ -48,7 +85,7 @@ static void timerSetup(void) {
     timer_enable_counter(TIM1);
 
     /** Timer 2 (Ch34) Setup */
-    /*Setup GPIO pins for used by the timer*/
+    /*Setup GPIO pins used by the timer*/
     rcc_periph_clock_enable(RCC_GPIOB);
     gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO10 | GPIO11);
     gpio_set_af(GPIOB, GPIO_AF1, GPIO10 | GPIO11);
@@ -72,7 +109,7 @@ static void timerSetup(void) {
     timer_enable_counter(TIM2);
 
     /** Timer 3 (Ch34) Setup */
-    /*Setup GPIO pins for used by the timer*/
+    /*Setup GPIO pins used by the timer*/
     rcc_periph_clock_enable(RCC_GPIOB);
     gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO0 | GPIO1);
     gpio_set_af(GPIOB, GPIO_AF2, GPIO0 | GPIO1);
@@ -96,7 +133,7 @@ static void timerSetup(void) {
     timer_enable_counter(TIM3);
 
     /** Timer 4 (Ch34) Setup */
-    /*Setup GPIO pins for used by the timer*/
+    /*Setup GPIO pins used by the timer*/
     rcc_periph_clock_enable(RCC_GPIOD);
     gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO14 | GPIO15);
     gpio_set_af(GPIOD, GPIO_AF2, GPIO14 | GPIO15);
