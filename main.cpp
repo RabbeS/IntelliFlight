4f21539b7b5438262d5a68ed6587b108ff38ebbd
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-09-23 00:43:38 +0200

    implement UART console with interrupts

--- main.cpp
+++ main.cpp
@@ -1,102 +1,367 @@
-#include "src/timer.h"
-#include "src/gpio.h"
 
-//TODO: Add UART_RX interrupt
-
-#include <libopencm3/stm32/rcc.h>
+//#include <stdint.h>
+#include <setjmp.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
-#include <libopencm3/lm4f/nvic.h>   //Nvic is from the wrong controller, missing stm32
+#include <libopencm3/stm32/f7/nvic.h>
+//#include <libopencm3/stm32/iwdg.h>
+//#include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/systick.h>
+#include "main.h"
 
-static void uartSetup(void);
 
-int main(void) {
-    gpioSetup();
-    timerSetup();
-    uartSetup();
-    while (true) {
-        for (int i = 0; i < 50000; i++) {}
-        gpio_toggle(GPIOC, GPIO13);
-        usart_send(UART7, 0xEA);
+/*
+ * Some definitions of our console "functions" attached to the
+ * USART.
+ *
+ * These define sort of the minimum "library" of functions which
+ * we can use on a serial port. If you wish to use a different
+ * USART there are several things to change:
+ *	- CONSOLE_UART change this
+ *	- Change the peripheral enable clock
+ *	- add usartx_isr for interrupts
+ *	- nvic_enable_interrupt(your choice of USART/UART)
+ *	- GPIO pins for transmit/receive
+ *		(may be on different alternate functions as well)
+ */
+
+/* milliseconds since boot */
+static volatile uint32_t system_millis;
+
+/* Called when systick fires */
+void sys_tick_handler(void)
+{
+    system_millis++;
+}
+
+/* simple sleep for delay milliseconds */
+void msleep(uint32_t delay)
+{
+    uint32_t wake = system_millis + delay;
+    while (wake > system_millis);
+}
+
+/* Getter function for the current time */
+uint32_t mtime(void)
+{
+    return system_millis;
+}
+
+/*
+ * clock_setup(void)
+ *
+ * This function sets up both the base board clock rate
+ * and a 1khz "system tick" count. The SYSTICK counter is
+ * a standard feature of the Cortex-M series.
+ */
+void clock_setup(void)
+{
+    /* Base board frequency, set to 168Mhz */
+//	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+    /* clock rate / 168000 to get 1mS interrupt rate */
+    systick_set_reload(168000);
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+    systick_counter_enable();
+
+    /* this done last */
+    systick_interrupt_enable();
+}
+
+
+#define CONSOLE_UART	UART7
+
+void console_putc(char c);
+char console_getc(int wait);
+void console_puts(char *s);
+int console_gets(char *s, int len);
+
+/* this is for fun, if you type ^C to this example it will reset */
+//#define RESET_ON_CTRLC
+
+#ifdef RESET_ON_CTRLC
+
+/* Jump buffer for setjmp/longjmp */
+jmp_buf	jump_buf;
+
+static void do_the_nasty(void);
+/*
+ * do_the_nasty
+ *
+ * This is a hack to implement the equivalent of a signal interrupt
+ * in a system without signals or a kernel or scheduler. Basically
+ * when the console_getc() function reads a '^C' character, it munges
+ * the return address of the interrupt to here, and then this function
+ * does a longjump to the last place we did a setjmp.
+ */
+static void do_the_nasty(void)
+{
+    longjmp(jump_buf, 1);
+    while (1);
+}
+#endif
+
+/* This is a ring buffer to holding characters as they are typed
+ * it maintains both the place to put the next character received
+ * from the UART, and the place where the last character was
+ * read by the program. See the README file for a discussion of
+ * the failure semantics.
+ */
+#define RECV_BUF_SIZE	128		/* Arbitrary buffer size */
+char recv_buf[RECV_BUF_SIZE];
+volatile int recv_ndx_nxt;		/* Next place to store */
+volatile int recv_ndx_cur;		/* Next place to read */
+
+/* For interrupt handling we add a new function which is called
+ * when recieve interrupts happen. The name (UART7_isr) is created
+ * by the irq.json file in libopencm3 calling this interrupt for
+ * UART7 'UART7', adding the suffix '_isr', and then weakly binding
+ * it to the 'do nothing' interrupt function in vec.c.
+ *
+ * By defining it in this file the linker will override that weak
+ * binding and instead bind it here, but you have to get the name
+ * right or it won't work. And you'll wonder where your interrupts
+ * are going.
+ */
+void uart7_isr(void)
+{
+    uint32_t	reg;
+    int			i;
+
+    do {
+        reg = USART_ISR(CONSOLE_UART);
+        if (reg & USART_FLAG_RXNE) {
+            recv_buf[recv_ndx_nxt] = USART_RDR(CONSOLE_UART);
+#ifdef RESET_ON_CTRLC
+            /* Check for "reset" */
+            if (recv_buf[recv_ndx_nxt] == '\003') {
+                /* reset the system volatile definition of
+                 * return address on the stack to insure it
+                 * gets stored, changed to point to the
+                 * trampoline function (do_the_nasty) which is
+                 * required because we need to return of an
+                 * interrupt to get the internal value of the
+                 * LR register reset and put the processor back
+                 * into "Thread" mode from "Handler" mode.
+                 *
+                 * See the PM0214 Programming Manual for Cortex
+                 * M, pg 42, to see the format of the Cortex M4
+                 * stack after an interrupt or exception has
+                 * occurred.
+                 */
+                volatile uint32_t *ret = (&reg) + 7;
+
+                *ret = (uint32_t) &do_the_nasty;
+                return;
+            }
+#endif
+            /* Check for "overrun" */
+            i = (recv_ndx_nxt + 1) % RECV_BUF_SIZE;
+            if (i != recv_ndx_cur) {
+                recv_ndx_nxt = i;
+            }
+        }
+    } while ((reg & USART_ISR_RXNE) != 0); /* can read back-to-back
+						 interrupts */
+}
+
+/*
+ * console_putc(char c)
+ *
+ * Send the character 'c' to the USART, wait for the USART
+ * transmit buffer to be empty first.
+ */
+void console_putc(char c)
+{
+    uint32_t	reg;
+    do {
+        reg = USART_ISR(CONSOLE_UART);
+    } while ((reg & USART_ISR_TXE) == 0);
+    USART_TDR(CONSOLE_UART) = (uint16_t) c & 0xff;
+}
+
+/*
+ * char = console_getc(int wait)
+ *
+ * Check the console for a character. If the wait flag is
+ * non-zero. Continue checking until a character is received
+ * otherwise return 0 if called and no character was available.
+ *
+ * The implementation is a bit different however, now it looks
+ * in the ring buffer to see if a character has arrived.
+ */
+char console_getc(int wait)
+{
+    char		c = 0;
+
+    while ((wait != 0) && (recv_ndx_cur == recv_ndx_nxt));
+    if (recv_ndx_cur != recv_ndx_nxt) {
+        c = recv_buf[recv_ndx_cur];
+        recv_ndx_cur = (recv_ndx_cur + 1) % RECV_BUF_SIZE;
+    }
+    return c;
+}
+
+/*
+ * void console_puts(char *s)
+ *
+ * Send a string to the console, one character at a time, return
+ * after the last character, as indicated by a NUL character, is
+ * reached.
+ */
+void console_puts(char *s)
+{
+    while (*s != '\000') {
+        console_putc(*s);
+        /* Add in a carraige return, after sending line feed */
+        if (*s == '\n') {
+            console_putc('\r');
+        }
+        s++;
     }
 }
 
-static void uartSetup(void) {
-    /** Uart 4 Setup */
-    /*Setup GPIO pins used by uart*/
-    rcc_periph_clock_enable(RCC_GPIOA);
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO0 | GPIO1);
-    gpio_set_af(GPIOA, GPIO_AF8, GPIO0 | GPIO1);
-    gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO0 | GPIO1);
+/*
+ * int console_gets(char *s, int len)
+ *
+ * Wait for a string to be entered on the console, limited
+ * support for editing characters (back space and delete)
+ * end when a <CR> character is received.
+ */
+int console_gets(char *s, int len)
+{
+    char *t = s;
+    char c;
 
-    /*Setup of the uart it selve*/
-    rcc_periph_clock_enable(RCC_UART4);
-    usart_set_baudrate(UART4, 115200);
-    usart_set_databits(UART4, 8);
-    usart_set_stopbits(UART4, USART_STOPBITS_1);
-    usart_set_mode(UART4, USART_MODE_TX_RX);
-    usart_set_parity(UART4, USART_PARITY_NONE);
-    usart_set_flow_control(UART4, USART_FLOWCONTROL_NONE);
-    usart_enable_halfduplex(UART4);     //Only used by the satellites
-    usart_enable(UART4);
-
-    /*Configuring interrupts*/
-    usart_enable_rx_interrupt(UART4);
-    nvic_enable_irq(NVIC_UART4_IRQ);
-
-    /** Uart 5 Setup */
-    /*Setup GPIO pins used by uart*/
-    rcc_periph_clock_enable(RCC_GPIOB);
-    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO12 | GPIO13);
-    gpio_set_af(GPIOB, GPIO_AF8, GPIO12 | GPIO13);
-    gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO12 | GPIO13);
-
-    /*Setup of the uart it selve*/
-    rcc_periph_clock_enable(RCC_UART5);
-    usart_set_baudrate(UART5, 115200);
-    usart_set_databits(UART5, 8);
-    usart_set_stopbits(UART5, USART_STOPBITS_1);
-    usart_set_mode(UART5, USART_MODE_TX_RX);
-    usart_set_parity(UART5, USART_PARITY_NONE);
-    usart_set_flow_control(UART5, USART_FLOWCONTROL_NONE);
-    usart_enable(UART5);
-
-    /** Uart 7 Setup */
-    /*Setup GPIO pins used by uart*/
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO7 | GPIO8);
-    gpio_set_af(GPIOE, GPIO_AF8, GPIO7 | GPIO8);
-    gpio_set_output_options(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO7 | GPIO8);
-
-    /*Setup of the uart it selve*/
-    rcc_periph_clock_enable(RCC_UART7);     //Change libopencm3 RCC_SPI7 -> RCC_UART7
-    usart_set_baudrate(UART7, 115200);
-    usart_set_databits(UART7, 8);
-    usart_set_stopbits(UART7, USART_STOPBITS_1);
-    usart_set_mode(UART7, USART_MODE_TX_RX);
-    usart_set_parity(UART7, USART_PARITY_NONE);
-    usart_set_flow_control(UART7, USART_FLOWCONTROL_NONE);
-    usart_enable(UART7);
-
-    /** Uart 8 Setup */
-    /*Setup GPIO pins used by uart*/
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_OTYPE_PP, GPIO0 | GPIO1);
-    gpio_set_af(GPIOE, GPIO_AF8, GPIO0 | GPIO1);
-    gpio_set_output_options(GPIOE, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO0 | GPIO1);
-
-    /*Setup of the uart it selve*/
-    rcc_periph_clock_enable(RCC_UART8);     //Change libopencm3 RCC_SPI8 -> RCC_UART8
-    usart_set_baudrate(UART8, 115200);
-    usart_set_databits(UART8, 8);
-    usart_set_stopbits(UART8, USART_STOPBITS_1);
-    usart_set_mode(UART8, USART_MODE_TX_RX);
-    usart_set_parity(UART8, USART_PARITY_NONE);
-    usart_set_flow_control(UART8, USART_FLOWCONTROL_NONE);
-    usart_enable_halfduplex(UART8);     //Only used by the satellites
-    usart_enable(UART8);
+    *t = '\000';
+    /* read until a <CR> is received */
+    while ((c = console_getc(1)) != '\r') {
+        if ((c == '\010') || (c == '\127')) {
+            if (t > s) {
+                /* send ^H ^H to erase previous character */
+                console_puts("\010 \010");
+                t--;
+            }
+        } else {
+            *t = c;
+            console_putc(c);
+            if ((t - s) < len) {
+                t++;
+            }
+        }
+        /* update end of string with NUL */
+        *t = '\000';
+    }
+    return t - s;
 }
 
-void uart7_isr(){
-//    uart_clear_interrupt_flag(UART0, serviced_irqs); //TODO: implement missing method, correct the nvic
+void countdown(void);
+
+/*
+ * countdown
+ *
+ * Count down for 20 seconds to 0.
+ *
+ * This provides an example function which is constantly
+ * printing for 20 seconds and not looking for typed characters.
+ * however with the interrupt driven receieve queue you can type
+ * ^C while it is counting down and it will be interrupted.
+ */
+void countdown(void)
+{
+    int i = 200;
+    while (i-- > 0) {
+        console_puts("Countdown: ");
+        console_putc((i / 600) + '0');
+        console_putc(':');
+        console_putc(((i % 600) / 100) + '0');
+        console_putc((((i % 600) / 10) % 10) + '0');
+        console_putc('.');
+        console_putc(((i % 600) % 10) + '0');
+        console_putc('\r');
+        msleep(100);
+    }
+}
+
+/*
+ * Set up the GPIO subsystem with an "Alternate Function"
+ * on some of the pins, in this case connected to a
+ * USART.
+ */
+int main(void)
+{
+    char buf[128];
+    int	len;
+    bool pmask;
+
+    clock_setup(); /* initialize our clock */
+
+    /* MUST enable the GPIO clock in ADDITION to the USART clock */
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    /* This example uses PA9 and PA10 for Tx and Rx respectively
+     * but other pins are available for this role on UART7 (our chosen
+     * USART) as well. We are using the ones mentioned above because they
+     * are connected to the programmer on the board through some jumpers.
+     */
+    gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO7 | GPIO8);
+
+    /* Actual Alternate function number (in this case 7) is part
+     * depenedent, CHECK THE DATA SHEET for the right number to
+     * use.
+     */
+    gpio_set_af(GPIOE, GPIO_AF8, GPIO7 | GPIO8);
+
+
+    /* This then enables the clock to the UART7 peripheral which is
+     * attached inside the chip to the APB1 bus. Different peripherals
+     * attach to different buses, and even some UARTS are attached to
+     * APB1 and some to APB2, again the data sheet is useful here.
+     */
+    rcc_periph_clock_enable(RCC_UART7);
+
+    /* Set up USART/UART parameters using the libopencm3 helper functions */
+    usart_set_baudrate(CONSOLE_UART, 115200);
+    usart_set_databits(CONSOLE_UART, 8);
+    usart_set_stopbits(CONSOLE_UART, USART_STOPBITS_1);
+    usart_set_mode(CONSOLE_UART, USART_MODE_TX_RX);
+    usart_set_parity(CONSOLE_UART, USART_PARITY_NONE);
+    usart_set_flow_control(CONSOLE_UART, USART_FLOWCONTROL_NONE);
+    usart_enable(CONSOLE_UART);
+
+    /* Enable interrupts from the USART */
+    nvic_enable_irq(NVIC_UART7_IRQ);
+
+    /* Specifically enable recieve interrupts */
+    usart_enable_rx_interrupt(CONSOLE_UART);
+
+    /* At this point our console is ready to go so we can create our
+     * simple application to run on it.
+     */
+    console_puts("\nUART Demonstration Application\n");
+#ifdef RESET_ON_CTRLC
+    console_puts("Press ^C at any time to reset system.\n");
+    pmask = cm_mask_interrupts(0);
+    cm_mask_interrupts(pmask);
+    if (setjmp(jump_buf)) {
+        console_puts("\nInterrupt received! Restarting from the top\n");
+    }
+#endif
+    while (1) {
+        console_puts("Enter a string: ");
+        len = console_gets(buf, 128);
+        if (len) {
+            if (buf[0] == 'c') {
+                console_puts("\n");
+                countdown();	/* long running thing (20
+						   seconds) */
+            }
+            console_puts("\nYou entered : '");
+            console_puts(buf);
+            console_puts("'\n");
+        } else {
+            console_puts("\nNo string entered\n");
+        }
+    }
 }
