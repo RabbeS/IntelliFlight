431030c4bdb81849e0891296eac72027017f34d5
Rabbe <Rabbe@Laptop>
2018-11-04 17:54:57 +0100

    add itm example

--- main.cpp
+++ main.cpp
@@ -1,133 +1,73 @@
 #ifndef INTELLIFLIGHT
 #define INTELLIFLIGHT
 
-#include <bit_utils.h>
 #include <openflightcontroller/board_defines.h>
-#include <bmp280.h>
-#include <openflightcontroller/spi.h>
 
-int counterIsr = 0;
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-void uart4_isr(void) {
-    counterIsr++;
-    uint32_t reg32 = USART_RDR(UART4);
+#include <libopencm3/stm32/dbgmcu.h>
+#include <libopencm3/cm3/scs.h>
+#include <libopencm3/cm3/tpiu.h>
+#include <libopencm3/cm3/itm.h>
+
+static void trace_setup(void) {
+    /* Enable trace subsystem (we'll use ITM and TPIU). */
+    SCS_DEMCR |= SCS_DEMCR_TRCENA;
+
+    /* Use Manchester code for asynchronous transmission. */
+    TPIU_SPPR = TPIU_SPPR_ASYNC_MANCHESTER;
+    TPIU_ACPR = 7;
+
+    /* Formatter and flush control. */
+    TPIU_FFCR &= ~TPIU_FFCR_ENFCONT;
+
+    /* Enable TRACESWO pin for async mode. */
+    DBGMCU_CR = DBGMCU_CR_TRACE_IOEN | DBGMCU_CR_TRACE_MODE_ASYNC;
+
+    /* Unlock access to ITM registers. */
+    /* FIXME: Magic numbers... Is this Cortex-M3 generic? */
+    *((volatile uint32_t *) 0xE0000FB0) = 0xC5ACCE55;
+
+    /* Enable ITM with ID = 1. */
+    ITM_TCR = (1 << 16) | ITM_TCR_ITMENA;
+    /* Enable stimulus port 1. */
+    ITM_TER[0] = 1;
 }
 
-void uart5_isr(void) {
-    counterIsr++;
-    uint32_t reg32 = USART_RDR(UART5);
+static void gpio_setup(void) {
+    /* Set GPIO13 (in GPIO port C) to 'output push-pull'. */
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_OTYPE_PP, GPIO13);
 }
 
-/* For interrupt handling we add a new function which is called
- * when recieve interrupts happen. The name (UART7_isr) is created
- * by the irq.json file in libopencm3 calling this interrupt for
- * UART7 'UART7', adding the suffix '_isr', and then weakly binding
- * it to the 'do nothing' interrupt function in vec.c.
- *
- * By defining it in this file the linker will override that weak
- * binding and instead bind it here, but you have to get the name
- * right or it won't work. And you'll wonder where your interrupts
- * are going.
- */
-void uart7_isr(void) {
-    counterIsr++;
-    uint32_t reg32 = USART_RDR(UART7);
+static void trace_send_blocking(char c) {
+    while (!(ITM_STIM8(0) & ITM_STIM_FIFOREADY));
+
+    ITM_STIM8(0) = c;
 }
-
-void uart8_isr(void) {
-    counterIsr++;
-    uint32_t reg32 = USART_RDR(UART8);
-}
-
-//TODO: outsource the handler to new file clock.c
-void sys_tick_handler(void) {
-    system_millis++;
-}
-
-
-bmp280_com_fptr_t bmp280_com_read = [](uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len) -> int8_t {
-    gpio_clear(BMP280_CSS_PORT, BMP280_CSS_GPIO);   //CSS (enable chip)
-
-    spi_send(BMP280_MAG_SPI, reg_addr); // request data
-    uint16_t answer = spi_read(BMP280_MAG_SPI);
-//    uint16_t answer2 = spi_read(BMP280_MAG_SPI);
-
-
-    // write data to receive buffer
-    for (int idx = 0; idx < len; idx++) {
-// TODO: here is a problem, this funktion is waiting to get an answer from the spi receive methode!
-        auto answer = spi_read(BMP280_MAG_SPI);
-        data[idx] = static_cast<uint8_t>(BIT_GET_SUFFIX(answer, 8));
-    }
-
-    gpio_set(BMP280_CSS_PORT, BMP280_CSS_GPIO);   //CSS (disable chip)
-
-    return BMP280_OK;
-};
-
-bmp280_com_fptr_t bmp280_com_write = [](uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len) -> int8_t {
-    gpio_clear(BMP280_CSS_PORT, BMP280_CSS_GPIO);   //CSS (enable chip)
-
-    // write data beginning with register `reg_addr`
-    for (uint8_t idx = 0; idx < len; idx++) {
-        spi_send(BMP280_MAG_SPI, static_cast<uint16_t> (reg_addr + idx));
-        spi_read(BMP280_MAG_SPI);
-        spi_send(BMP280_MAG_SPI, data[idx]);
-        spi_read(BMP280_MAG_SPI);
-    }
-
-    gpio_set(BMP280_CSS_PORT, BMP280_CSS_GPIO);   //CSS (disable chip)
-
-    return BMP280_OK;
-};
-
-
-bmp280_delay_fptr_t bmp280_delay = [](uint32_t period) -> void {
-//    msleep(period);
-};
 
 int main(void) {
+    int i, j = 0, c = 0;
+
     clockSetup();
-    gpioSetup();
-//    timerSetup();
-    spi_setup();
-    uartSetup();
+    gpio_setup();
+    trace_setup();
 
-    int8_t rslt;
-    bmp280_dev bmp;
-
-/* Sensor interface over SPI with native chip select line */
-    bmp.dev_id = 0;
-    bmp.intf = BMP280_SPI_INTF;
-    bmp.read = bmp280_com_read;
-    bmp.write = bmp280_com_write;
-    bmp.delay_ms = bmp280_delay;
-
-    bmp280_config conf;
-    conf.spi3w_en = BMP280_SPI3_WIRE_DISABLE;
-    conf.os_pres = BMP280_OS_16X;
-    conf.os_temp = BMP280_OS_2X;
-    conf.filter = BMP280_FILTER_COEFF_16;
-    conf.odr = BMP280_ODR_0_5_MS;
-
-//    rslt = bmp280_init(&bmp);
-//    rslt = bmp280_set_config(&conf, &bmp);
-
-    while (true) {
-//        gpio_clear(GPIOA, GPIO2);
-//        spi_send(SPI4, 0xD0);
-//        uint16_t answer = spi_read(SPI4);
-//        uint16_t answer2 = spi_read(SPI4);
-//        gpio_set(GPIOA, GPIO2);
-
-//        int8_t answer = bmp280_get_regs(0xD0, 0x0, 0, &bmp);
-
-        gpio_toggle(GPIOC, GPIO13);
-        for (int i = 0; i < 500000; i++) {}
-        if(counterIsr >= 5){
-            gpio_set(GPIOE, GPIO14);
+    /* Blink the LED (PC13) on the board with every transmitted byte. */
+    while (1) {
+        gpio_toggle(GPIOC, GPIO13);    /* LED on/off */
+        trace_send_blocking(c + '0');
+        c = (c == 9) ? 0 : c + 1;    /* Increment c. */
+        if ((j++ % 80) == 0) {        /* Newline after line full. */
+            trace_send_blocking('\r');
+            trace_send_blocking('\n');
         }
+        for (i = 0; i < 800000; i++)    /* Wait a bit. */
+                __asm__("nop");
     }
+
+    return 0;
 }
 
 #endif //INTELLIFLIGHT
